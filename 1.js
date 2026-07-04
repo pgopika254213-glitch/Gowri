d2233f1ef7dc131c1a3c2073dcb3371e89109004
Gowri Gopika <pgopika254213@sasi.ac.in>
2026-07-04 09:44:47 +0530

    Add files via upload

---
+++ 1.js
@@ -0,0 +1,71 @@
+console.log("GOWRI");
+//console.log(GOWRI);
+console.log(5);
+console.log("TRUE");
+const n=7;
+console.log(n);
+//n=3;
+{
+    const b=20;
+    console.log(20);
+}
+//console.log(b)  
+{
+    var c=30;
+    console.log(c);
+}
+console.log(c);
+{
+    let d=40;
+    console.log(d);
+}
+d="gowri"
+console.log(d);
+d=7
+console.log(d);
+d=7.9
+console.log(d);
+d="true"
+console.log(d);
+{
+    var e=70
+    console.log(e);
+}
+e="gowri"
+console.log(e);
+e=77
+console.log(e);
+e=7.90
+console.log(e);
+let i=0;
+do{
+    console.log(i);
+    i++
+}
+while(i<=5) 
+let j=5;
+do{
+    console.log(j);
+    j++
+}
+while(j<5) 
+let arr=[1,"Gowri","gnpm",40,50]
+console.log(arr);
+
+for(let x of arr){
+    console.log(x);
+}
+let obj={
+    "key":"value",
+    "name":"Gowri",
+    "city":"Gnpm"
+}
+for(let x in obj){
+    console.log(x);
+    //console.log(obj[x]);
+    console.log(x+":"+obj[x]);
+}
+var arrobj=[["Name":"gowri","age":17],["Name":"gopika","Age":18]]
+for(let x of arrobj){
+    
+}
---
+++ dom.js
@@ -0,0 +1,4 @@
+document.getElementById("heading1").innerHTML="DOM";
+let p=document.getElementById("paragraph1");
+p.innerHTML="DOM is DOCUMENT OBJECT MODEL";
+console.log(document)
