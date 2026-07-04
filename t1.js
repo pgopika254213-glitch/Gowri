d8d5561d882eb0bd7dbdc6f60b0ff3da2537bab2
Gowri Gopika <pgopika254213@sasi.ac.in>
2026-07-04 10:33:36 +0530

    Delete t1.js

--- t1.js
+++
@@ -1,9 +0,0 @@
-let arr=[1,2,3,4,6,10]
-for (let i=0;i<arr.length;i++) {
-    if(arr[i]%2===0) {
-        console.log(arr[i]+"is Even");
-    } 
-    else {
-        console.log(arr[i]+"is Odd");
-    }
-}
